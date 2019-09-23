0945a2229b8d3ce7fd3f86ccba15f2f932135272
Dominic Torruellas <dom@domtorr.com>
2019-09-22 21:31:39 -0700

    2.3.3 - Function Arity

---
+++ 02-Building-Blocks/2.3.3-Function-arity/02_arity_calc.ex
@@ -0,0 +1,15 @@
+# defmodule Calculator do
+#   def sum(a) do
+#     sum(a, 0)
+#   end
+
+#   def sum(a, b) do
+#     a + b
+#   end
+# end
+
+defmodule Calculator do
+  def sum(a, b \\ 0) do
+    a + b
+  end
+end
