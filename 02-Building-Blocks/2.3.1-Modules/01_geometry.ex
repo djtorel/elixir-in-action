f2ddb082edb9f9621dfabcaeefe06ff2109afba1
Dominic Torruellas <dom@domtorr.com>
2019-09-22 21:31:24 -0700

    2.3.1 - Modules

---
+++ 02-Building-Blocks/2.3.1-Modules/01_geometry.ex
@@ -0,0 +1,13 @@
+# defmodule Geometry do
+#   def rectangle_area(a, b), do: a * b
+# end
+
+defmodule Geometry do
+  def rectangle_area(a, b) do
+    a * b
+  end
+
+  def square_area(a) do
+    rectangle_area(a, a)
+  end
+end
